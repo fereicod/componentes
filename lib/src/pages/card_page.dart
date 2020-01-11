136b1da8625c774407f663a16b88081d07fc3eca
Fernando E.I <mferna.92@gmail.com>
2020-01-10 23:15:47 -0600

    create pages

---
+++ lib/src/pages/card_page.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class CardPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cards'),
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(10.0),
+        children: <Widget>[
+          _cardTipo1()
+        ],
+      ),
+    );
+  }
+
+  Widget _cardTipo1(){
+    
+    return Card(
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            title: Text("Card TIPO 1"),
+            subtitle: Text("Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen."),
+            leading: Icon(Icons.phone_android),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              FlatButton(
+                child: Text('Cancelar'),
+                onPressed: (){
+
+                },
+              ),
+              FlatButton(
+                child: Text('OK'),
+                onPressed: (){
+
+                },
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
